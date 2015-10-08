ddc10b3a0d73691bf7563027227a44398b735e6f
ShostakRV <morfiy.roma@gmail.com>
2015-10-09 00:10:39 +0300

    Base setup

--- src/main/java/com/my/application/black/jack/web/controller/TestRestController.java
+++ src/main/java/com/my/application/black/jack/web/controller/TestRestController.java
@@ -1,9 +1,6 @@
 package com.my.application.black.jack.web.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Developer: Roman Shostak
@@ -20,5 +17,40 @@ public class TestRestController {
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String helloWorld() {
         return "helloWorld ";
+    }
+
+    @RequestMapping(value = "/testDto", method = RequestMethod.GET)
+    public SomeDto testDto(@RequestParam(value = "name") String name) {
+        return new SomeDto(1, name);
+    }
+
+
+    public static class SomeDto {
+        private long id;
+        private String name;
+
+        public SomeDto() {
+        }
+
+        public SomeDto(long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
