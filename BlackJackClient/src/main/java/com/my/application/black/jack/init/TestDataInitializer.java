38a3539c48c043dca550300f2d30c611dd3357a0
ShostakRV <morfiy.roma@gmail.com>
2016-01-03 14:57:40 +0200

    Restructure

--- BlackJackClient/src/main/java/com/my/application/black/jack/init/TestDataInitializer.java
+++ BlackJackClient/src/main/java/com/my/application/black/jack/init/TestDataInitializer.java
@@ -16,11 +16,11 @@ import java.util.Date;
 /**
  *
  * This is a initializing bean that inserts some test data in the database. It is only active in
- * the development profile, to see the data login with user123 / PAssword2 and do a search starting on
+ * the development profile, to see the data login with user123 / Password2 and do a search starting on
  * 1st of January 2015.
  *
  */
-//@Component
+
 public class TestDataInitializer {
 
     @Autowired
