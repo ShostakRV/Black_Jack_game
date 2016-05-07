827c6733438ac3af463c5a9e986ea53602b5fc09
ShostakRV <morfiy.roma@gmail.com>
2016-05-07 18:26:11 +0300

    Post use example

--- BlackJackClient/src/main/java/com/my/application/black/jack/run/config/security/AppSecurityConfig.java
+++ BlackJackClient/src/main/java/com/my/application/black/jack/run/config/security/AppSecurityConfig.java
@@ -3,13 +3,20 @@ package com.my.application.black.jack.run.config.security;
 import com.my.application.black.jack.server.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 /**
  * The Spring Security configuration for the application - its a form login config with authentication via session cookie (once logged in),
@@ -19,72 +26,49 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
  */
 @Configuration
 @EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final Logger LOGGER = Logger.getLogger( AppSecurityConfig.class );
+    private static final Logger LOGGER = Logger.getLogger(AppSecurityConfig.class);
 
-	@Autowired
-	private SecurityUserDetailsService securityUserDetailsService;
+    @Autowired
+    private SecurityUserDetailsService securityUserDetailsService;
 
-	@Override
-	protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
-		auth.userDetailsService( securityUserDetailsService ).passwordEncoder( new BCryptPasswordEncoder() );
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
-	@Override
-	protected void configure( HttpSecurity http ) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests()
-			.antMatchers( "/", "/home", "/static/**", "/css/**" ).permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.loginPage( "/login" )
-			.permitAll()
-			.and()
-			.logout()
-			.permitAll();
-		//        test(http);
+        http.authorizeRequests()
+                .antMatchers("/", "/home", "/static/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and().csrf().csrfTokenRepository(csrfTokenRepository())
+                .and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
 
 
-	}
+        ;
+    }
 
-	private void test( HttpSecurity http ) throws Exception {
-		//        CsrfTokenResponseHeaderBindingFilter csrfTokenFilter = new CsrfTokenResponseHeaderBindingFilter();
-		http.authorizeRequests()
-			//        http.addFilterAfter(csrfTokenFilter, CsrfFilter.class);
-			.antMatchers( "/index.html", "/index", "/", "/index2" ).permitAll()
-			//                .antMatchers("/game/**").permitAll()
-			.antMatchers( "/authorization/**" ).permitAll()
-			.antMatchers( "/resources/img/**" ).permitAll()
-			.antMatchers( "/resources/js/**" ).permitAll()
-			.antMatchers( "/resources/css/**" ).permitAll()
-			.antMatchers( "/resources/bower_components/**" ).permitAll()
-			.antMatchers( HttpMethod.POST, "/user" ).permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.defaultSuccessUrl( "/index" )
-			.loginProcessingUrl( "/authenticate" )
-			.usernameParameter( "username" )
-			.passwordParameter( "password" )
-			.successHandler( new AjaxAuthenticationSuccessHandler( new SavedRequestAwareAuthenticationSuccessHandler() ) )
-			//                .loginPage("/public/login.html")
-			.and()
-			.httpBasic()
-			.and()
-			.logout()
-			.logoutUrl( "/logout" )
-			.logoutSuccessUrl( "/index.html" )
-			.permitAll();
-		if ( "true".equals( System.getProperty( "httpsOnly" ) ) ) {
-			LOGGER.info( "launching the application in HTTPS-only mode" );
-			http.requiresChannel().anyRequest().requiresSecure();
-		}
-	}
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
 
-	@Bean
-	public SecurityUserDetailsService getSecurityUserDetailsService( UserService userService ) {
-		return new SecurityUserDetailsService( userService );
-	}
+    @Bean
+    public SecurityUserDetailsService getSecurityUserDetailsService(UserService userService) {
+        return new SecurityUserDetailsService(userService);
+    }
 }
